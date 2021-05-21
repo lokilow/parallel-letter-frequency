f5b127bbf3afdc9ccbd2793fdd4cb3542bf5d441
lokilow <lokilow@protonmail.com>
2021-05-21 17:04:41 +0000

    pass all tests synchronously

--- lib/frequency.ex
+++ lib/frequency.ex
@@ -8,5 +8,30 @@ defmodule Frequency do
   """
   @spec frequency([String.t()], pos_integer) :: map
   def frequency(texts, workers) do
+    _ = workers
+
+    texts
+    |> Enum.map(&string_freq/1)
+    |> Enum.reduce(%{}, fn m1, m2 ->
+      Map.merge(m1, m2, fn _k, v1, v2 -> v1 + v2 end)
+    end)
+  end
+
+  @invalid_characters ~r/[ 0-9\.\?\-\(\)\[\]\{\}\|,.'":;!@#%&*_=]+/
+  defp string_freq(text) when is_binary(text) do
+    # clean text
+    text = String.replace(text, @invalid_characters, "")
+    string_freq(text, %{})
+  end
+
+  defp string_freq(s, acc) do
+    case String.next_grapheme(s) do
+      nil ->
+        acc
+
+      {char, rest} ->
+        char = String.downcase(char)
+        string_freq(rest, acc |> Map.update(char, 1, &(&1 + 1)))
+    end
   end
 end
